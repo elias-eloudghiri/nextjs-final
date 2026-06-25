7d8f3c1a1c0ca69e5d1e9c196200e796d101b63a
elias <elias.eloudghiri@gmail.com>
2026-06-25 15:07:12 +0200

    debut page job seul

--- app/job/[uid]/page.tsx
+++ app/job/[uid]/page.tsx
@@ -1,11 +1,24 @@
 import { SliceZone } from "@prismicio/react";
 import { createClient } from "@/prismicio";
 import { components } from "@/slices";
+import MainTitle from "@/components/ui/MainTitle";
+import RedirectButton from "@/components/ui/RedirectButton";
 
 export default async function Page({ params }: PageProps<"/job/[uid]">) {
-	const { uid } = await params;
-	const client = createClient();
-	const page = await client.getByUID("job", uid);
+  const { uid } = await params;
+  const client = createClient();
+  const page = await client.getByUID("job", uid);
 
-	return <SliceZone slices={page.data.slices} components={components} />;
+  return (
+    <main>
+      <div className="pl-15 pr-15 pt-10">
+        <RedirectButton
+          content="< Voir toutes les offres"
+          redirectUrl="/jobs"
+        />
+        <MainTitle content={page.data.title} countOffre={0} />
+        <SliceZone slices={page.data.slices} components={components} />
+      </div>
+    </main>
+  );
 }
--- components/ui/JobCard.tsx
+++ components/ui/JobCard.tsx
@@ -1,36 +1,44 @@
 import { Job } from "@/types/job";
+import { redirect } from "next/dist/server/api-utils";
 import Image from "next/image";
 
 type JobCardType = {
   job: {
     uid: string;
     data: Job;
-  }
+  };
 };
 
 export default function JobCard({ job }: JobCardType) {
   return (
-    <div className="job-card">
-      <div className="flex justify-between">
-        <label>{job.data.title}</label>
-        <Image src={"/save.svg"} alt="bookmark" width={20} height={20} />
+    <a href={"/job/" + job.uid}>
+      <div className="job-card">
+        <div className="flex justify-between">
+          <label>{job.data.title}</label>
+          <Image src={"/save.svg"} alt="bookmark" width={20} height={20} />
+        </div>
+        <div className="flex">
+          <Image
+            src={"/calendarBlue.svg"}
+            alt="calendar"
+            width={20}
+            height={20}
+          />
+          <p>{job.data.date}</p>
+        </div>
+        <div className="flex">
+          <Image
+            src={"/bracketBlue.svg"}
+            alt="bracket"
+            width={20}
+            height={20}
+          />
+          <p>{job.data.techno}</p>
+        </div>
+        <div>
+          <p>{job.data.description_card}</p>
+        </div>
       </div>
-      <div className="flex">
-        <Image
-          src={"/calendarBlue.svg"}
-          alt="calendar"
-          width={20}
-          height={20}
-        />
-        <p>{job.data.date}</p>
-      </div>
-      <div className="flex">
-        <Image src={"/bracketBlue.svg"} alt="bracket" width={20} height={20} />
-        <p>{job.data.techno}</p>
-      </div>
-      <div>
-        <p>{job.data.description_card}</p>
-      </div>
-    </div>
+    </a>
   );
 }
